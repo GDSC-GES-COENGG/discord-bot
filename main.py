8b24205cc0b13c9f5f5fc830be79ac64b9cc6c1f
Atharva Chandwadkar <70438130+atharva21-stack@users.noreply.github.com>
2021-09-12 09:39:53 +0000

    Basic setup

---
+++ main.py
@@ -0,0 +1,22 @@
+import discord
+import os
+
+client = discord.Client()
+
+@client.event
+
+async def on_ready():
+    print('We have logged in as {0.user}'.format(client))
+
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+      return
+    
+    if message.content.startwith('!hello'):
+      await message.channel.send('Hello')
+
+client.run(os.getenv('TOKEN'))
+
+my_secret = os.environ['TOKEN']
